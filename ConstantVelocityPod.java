b335270f75500b895f1fe9b44a15a1e4fd522977
Jodi Francis <jodiannfrancis3@gmail.com>
2020-04-07 15:57:05 -0500

    update

--- ConstantVelocityPod.java
+++ ConstantVelocityPod.java
@@ -2,4 +2,7 @@ public class ConstantVelocityPod extends Pod {
     public ConstantVelocityPod(String name, double velocity) {
         super(name, t -> velocity);
     }
+    public double distancedTraveled(double startTime, double endTime) {
+      return (endTime - startTime) * (v(startTime) + v(endTime));
+}
 }
